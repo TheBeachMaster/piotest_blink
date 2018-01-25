c4c1e5f912765049627beffdd7e4ae219a6f4a36
TheBeachmaster <otienoken@outlook.com>
2018-01-25 15:29:37 +0300

    Add test cases for desktop,generic and mega clients

---
+++ test/mega/test_mega.cpp
@@ -0,0 +1,53 @@
+#include <Arduino.h>
+#include "calc.h"
+#include "unity.h"
+
+#ifndef UNIT_TEST
+
+Calculator calc;
+
+void test_add(void)
+{
+    TEST_ASSERT_EQUAL(15,calc.add(7,8));
+}
+
+void test_multiplication(void)
+{
+    TEST_ASSERT_EQUAL(6,calc.multiply(3,2));
+}
+
+void test_square(void)
+{
+    TEST_ASSERT_EQUAL(16,calc.square(4));
+}
+
+void test_division(void)
+{
+    TEST_ASSERT_EQUAL(8,calc.divide(16,2));
+}
+
+void test_subtraction(void)
+{
+    TEST_ASSERT_EQUAL(10,calc.subtract(100,90));
+}
+
+void setup(void)
+{
+    UNITY_BEGIN();
+    RUN_TEST(test_add);
+    RUN_TEST(test_multiplication);
+    RUN_TEST(test_square);
+    RUN_TEST(test_division);
+    RUN_TEST(test_subtraction);
+    UNITY_END();
+}
+
+void loop(void)
+{
+    digitalWrite(13, HIGH);
+    delay(100);
+    digitalWrite(13, LOW);
+    delay(500);
+}
+
+#endif // !UNIT_TEST
