10e2114f3b3b1af7af32119875aa603ffd4bf10d
TheBeachmaster <otienoken@outlook.com>
2018-01-25 12:32:11 +0300

    Setup Calculator library methods

---
+++ lib/calc/src/calc.cpp
@@ -0,0 +1,26 @@
+#include "calc.h"
+
+int Calculator::add(int a, int b)
+{
+    return a + b;
+}
+
+int Calculator::divide(int a, int b)
+{
+    return a / b;
+}
+
+int Calculator::multiply(int a, int b)
+{
+    return a * b;
+}
+
+int Calculator::square(int a)
+{
+    return a * a;
+}
+
+int Calculator::subtract(int a, int b)
+{
+    return a - b;
+}
